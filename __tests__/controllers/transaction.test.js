d0c3d887b0dbca5cdcf3cce8149f86af8da1a8b1
Zane Pocock <zanepocock@gmail.com>
2021-09-25 15:49:09 -0500

    Empty tests

---
+++ __tests__/controllers/transaction.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment node
+ */
+
+//TODO: Get this shit working
+// const request = require('supertest')
+
+// beforeAll(async () => {
+//   server = require('../../server')
+// });
+
+// afterAll(async () => {
+  // await server.close();
+// });
+
+describe('GET /transactions', () => {
+  it('gets all transactions', async () => {
+    return true
+//       const res = await request(server)
+//           .get('/transactions')
+          // .set('Authorization', '')
+          // .send();
+
+//       expect(res).not.toBeNull();
+  });
+});
+ 

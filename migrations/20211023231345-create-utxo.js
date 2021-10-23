94f1e1712d6740c8cae4059c0e193b92b7e4875d
Zane Pocock <zanepocock@gmail.com>
2021-10-23 18:35:05 -0500

    create utxo model, migration

---
+++ migrations/20211023231345-create-utxo.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('utxos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      utxo: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('utxos');
+  }
+};
---
+++ models/UTXO.js
@@ -0,0 +1,23 @@
+const {
+  Model
+} = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class Utxo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Utxo.belongsTo(models.transactionledger)
+    }
+  };
+  Utxo.init({
+    utxo: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'utxo',
+  });
+  return Utxo;
+};

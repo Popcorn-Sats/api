3ef0168f84478bd2eb9d9d964b36c4058fae258b
Zane Pocock <zanepocock@gmail.com>
2021-10-24 19:50:11 -0500

    Reorder migrations, fix ledger seeder

--- seeders/20211024164356-demo-ledgers.js
+++ seeders/20211024164356-demo-ledgers.js
@@ -1,3 +1,5 @@
+const { date } = require("faker");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
       await queryInterface.bulkInsert('transactionledgers', [
@@ -5,73 +7,109 @@ module.exports = {
         accountId: 6,
         transactionId: 1,
         transactiontypeId: 2,
-        address: null
+        address: null,
+        amount: 42098,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 1,
         transactionId: 1,
         transactiontypeId: 1,
-        address: null
+        address: null,
+        amount: 42098,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 7,
         transactionId: 2,
         transactiontypeId: 2,
-        address: null
+        address: null,
+        amount: 980765,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 1,
         transactionId: 2,
         transactiontypeId: 1,
-        address: null
+        address: null,
+        amount: 980765,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 1,
         transactionId: 3,
         transactiontypeId: 2,
-        address: null
+        address: null,
+        amount: 8932345,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 8,
         transactionId: 3,
         transactiontypeId: 1,
-        address: null
+        address: null,
+        amount: 8932345,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 25,
         transactionId: 38,
         transactiontypeId: 2,
-        address: null
+        address: null,
+        amount: 458792345,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 22,
         transactionId: 38,
         transactiontypeId: 1,
-        address: null
+        address: null,
+        amount: 458792345,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: null,
         transactionId: 5,
         transactiontypeId: 2,
-        address: null
+        address: 'bc1ab23cd45',
+        amount: 920,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 1,
         transactionId: 5,
         transactiontypeId: 1,
-        address: null
+        address: null,
+        amount: 920,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 25,
         transactionId: 40,
         transactiontypeId: 2,
-        address: null
+        address: null,
+        amount: 1080339,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         accountId: 22,
         transactionId: 40,
         transactiontypeId: 1,
-        address: null
+        address: null,
+        amount: 1080339,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       ], {});
     

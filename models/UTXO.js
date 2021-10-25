0a3270203161e7646426a4c0505e6d3cb7068be5
Zane Pocock <zanepocock@gmail.com>
2021-10-24 19:09:20 -0500

    UTXO model belongs to address

--- models/UTXO.js
+++ models/UTXO.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Utxo.belongsTo(models.transactionledger)
+      Utxo.belongsTo(models.address)
     }
   };
   Utxo.init({

c4acec9493ebaa147c653cdee511c8926c7c4d5f
Zane Pocock <zanepocock@gmail.com>
2021-11-21 12:12:31 -0600

    getAddressTransactions electrum method

---
+++ scripts/getAddressTransactions.js
@@ -0,0 +1,9 @@
+const {getAddressTransactions} = require('../services/electrum')
+
+const address = process.argv[2];
+
+(async() => {
+  console.log({address});
+  const result = await getAddressTransactions(address)
+  console.log({result});
+})();
--- services/electrum.js
+++ services/electrum.js
@@ -3,73 +3,108 @@ const Bitcoin = require('bitcoinjs-lib')
 const config = require('../config/config.json')
 const ElectrumClient = require('../electrum-client')
 
-  /* const electrumCallbacks = {
-    onConnect: (client, versionInfo) => { logger.info(`Connected to Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT} (${JSON.stringify(versionInfo)})`); },
-    onClose: (client) => { logger.info(`Disconnected from Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT}`); },
-    onError: (err) => { logger.err(`Electrum error: ${JSON.stringify(err)}`); },
-    onLog: (str) => { logger.debug(str); },
-  }; */
-  
-  const electrumConfig = { client: 'mempool-v2', version: '1.4' };
-  const electrumPersistencePolicy = { retryPeriod: 10000, maxRetry: 1000, callback: null };
-  
-  const electrumClient = new ElectrumClient(
-    config.ELECTRUM.PORT,
-    config.ELECTRUM.HOST,
-    config.ELECTRUM.TLS_ENABLED ? 'tls' : 'tcp',
-    null,
-    /* electrumCallbacks */
-  )
-  
-  /* const initiate = electrumClient.initElectrum(electrumConfig, electrumPersistencePolicy)
-        .then(() => {})
-        .catch((err) => {
-          console.error(`Error connecting to Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT}. Error: `, err);
-        }); */
+const electrumCallbacks = {
+  onConnect: (client, versionInfo) => { console.log(`Connected to Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT} (${JSON.stringify(versionInfo)})`); },
+  onClose: (client) => { console.log(`Disconnected from Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT}`); },
+  onError: (err) => { console.error(`Electrum error: ${JSON.stringify(err)}`); },
+  onLog: (str) => { console.debug(str); },
+};
 
-  const getScriptHashBalance = (scripthash) => electrumClient.blockchainScripthash_getBalance(scripthash)
+const electrumConfig = { client: 'mempool-v2', version: '1.4' };
+const electrumPersistencePolicy = { retryPeriod: 10000, maxRetry: 1000, callback: null };
 
-  const getAddress = async (address) => {
-    electrumClient.initElectrum(electrumConfig, electrumPersistencePolicy)
-        .then(() => {})
-        .catch((err) => {
-          console.error(`Error connecting to Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT}. Error: `, err);
-        });
-    try {
-      const script = Bitcoin.address.toOutputScript(address)
-      const hash = Bitcoin.crypto.sha256(script)
-      const reversedHash = hash.reverse().toString('hex')
+const electrumClient = new ElectrumClient(
+  config.ELECTRUM.PORT,
+  config.ELECTRUM.HOST,
+  config.ELECTRUM.TLS_ENABLED ? 'tls' : 'tcp',
+  null,
+  electrumCallbacks
+)
 
-      const balance = await getScriptHashBalance(reversedHash)
-      // const history = await electrumClient.getScriptHashHistory(reversedHash)
+const initiate = () => {
+  electrumClient.initElectrum(electrumConfig, electrumPersistencePolicy)
+    .then(() => {})
+    .catch((err) => {
+      console.error(`Error connecting to Electrum Server at ${config.ELECTRUM.HOST}:${config.ELECTRUM.PORT}. Error: `, err);
+    });
+} 
 
-      // const unconfirmed = history.filter((h) => h.fee).length
+const getScriptHashBalance = (scripthash) => electrumClient.blockchainScripthash_getBalance(scripthash)
+const getScriptHashHistory = (scripthash) => electrumClient.blockchainScripthash_getHistory(scripthash) // TODO: cache this
+const getRawTransaction = (txHash, verbosity) => electrumClient.blockchainTransaction_get(txHash, verbosity)
 
-      return {
-        'address': address,
-        'chain_stats': {
-          'funded_txo_count': 0,
-          'funded_txo_sum': balance.confirmed ? balance.confirmed : 0,
-          'spent_txo_count': 0,
-          'spent_txo_sum': balance.confirmed < 0 ? balance.confirmed : 0,
-          // 'tx_count': history.length - unconfirmed,
-        },
-        'mempool_stats': {
-          'funded_txo_count': 0,
-          'funded_txo_sum': balance.unconfirmed > 0 ? balance.unconfirmed : 0,
-          'spent_txo_count': 0,
-          'spent_txo_sum': balance.unconfirmed < 0 ? -balance.unconfirmed : 0,
-          // 'tx_count': unconfirmed,
-        },
-        'electrum': true,
-      };
+const getAddress = async (address) => {
+  initiate()
+  try {
+    const script = Bitcoin.address.toOutputScript(address)
+    const hash = Bitcoin.crypto.sha256(script)
+    const reversedHash = hash.reverse().toString('hex')
 
-    } catch (e) {
-      console.error(e)
-      return({"Error": e})
-    }
+    const balance = await getScriptHashBalance(reversedHash)
+    const history = await getScriptHashHistory(reversedHash)
+
+    const unconfirmed = history.filter((h) => h.fee).length
+
+    return {
+      'address': address,
+      'chain_stats': {
+        'funded_txo_count': 0,
+        'funded_txo_sum': balance.confirmed ? balance.confirmed : 0,
+        'spent_txo_count': 0,
+        'spent_txo_sum': balance.confirmed < 0 ? balance.confirmed : 0,
+        'tx_count': history.length - unconfirmed,
+      },
+      'mempool_stats': {
+        'funded_txo_count': 0,
+        'funded_txo_sum': balance.unconfirmed > 0 ? balance.unconfirmed : 0,
+        'spent_txo_count': 0,
+        'spent_txo_sum': balance.unconfirmed < 0 ? -balance.unconfirmed : 0,
+        'tx_count': unconfirmed,
+      },
+      'electrum': true,
+    };
+
+  } catch (e) {
+    console.error(e)
+    return({"Error": e})
   }
+}
+
+const getAddressTransactions = async (address, lastSeenTxId) => {
+  initiate()
+  try {
+    // TODO: pull from Bitcoin service
+    const script = Bitcoin.address.toOutputScript(address)
+    const hash = Bitcoin.crypto.sha256(script)
+    const reversedHash = hash.reverse().toString('hex')
+
+    const transactions = []
+    const history = await getScriptHashHistory(reversedHash)
+    history.sort((a, b) => (b.height || 9999999) - (a.height || 9999999))
+
+    let startingIndex = 0;
+    if (lastSeenTxId) {
+      const pos = history.findIndex((historicalTx) => historicalTx.tx_hash === lastSeenTxId);
+      if (pos) {
+        startingIndex = pos + 1;
+      }
+    }
+    const endIndex = Math.min(startingIndex + 10, history.length)
+
+    for (let i = startingIndex; i < endIndex; i++) {
+      const tx = await getRawTransaction(history[i].tx_hash, true)
+      transactions.push(tx)
+    }
+
+    return transactions;
+
+  } catch (e) {
+    console.error(e)
+    return({"Error": e})
+  }
+}
 
 module.exports = {
   getAddress,
+  getAddressTransactions
 }

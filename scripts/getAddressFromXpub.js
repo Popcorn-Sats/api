84708739bbec1e05d237520f6bbfc986ef685c5c
Zane Pocock <zanepocock@gmail.com>
2021-12-08 22:00:49 -0600

    Prepare to take change addresses

--- scripts/getAddressFromXpub.js
+++ scripts/getAddressFromXpub.js
@@ -5,11 +5,12 @@ const extPubKey = process.argv[2];
 const keyIndex = process.argv[3];
 const purpose = process.argv[4];
 const accountNumber = process.argv[5];
+const change = process.argv[6];
 
 // Purpose can be: `P2PKH` (m / 44' /), `P2SH` (m / 49' /), `P2WPKH` (m / 84' /)
 
 (async() => {
   console.log({extPubKey});
-  const result = await getAddressFromXpub(extPubKey, keyIndex, purpose, accountNumber)
+  const result = await getAddressFromXpub(extPubKey, keyIndex, purpose, accountNumber, change)
   console.log({result});
 })();
--- services/bitcoin.js
+++ services/bitcoin.js
@@ -2,6 +2,8 @@
 const Bitcoin = require('bitcoinjs-lib')
 const { addressFromExtPubKey, addressesFromExtPubKey } = require('@swan-bitcoin/xpub-lib')
 const { Purpose } = require('@swan-bitcoin/xpub-lib/lib/purpose')
+/* const { addressFromExtPubKey, addressesFromExtPubKey } = require('../xpub-tool/packages/xpub-lib/src')
+const { Purpose } = require('../xpub-tool/packages/xpub-lib/src/purpose') */
 
 module.exports.getScriptHash = (address) => {
   const script = Bitcoin.address.toOutputScript(address)
@@ -12,7 +14,7 @@ module.exports.getScriptHash = (address) => {
   return reversedHash
 }
 
-module.exports.getAddressFromXpub = (extPubKey, keyIndex, purpose, accountNumber) => {
+module.exports.getAddressFromXpub = (extPubKey, keyIndex, purpose, accountNumber, change) => {
 
   // FIXME: Need to define receive/change chain. Defaults to Receive
   // Purpose can be: `P2PKH` (m / 44' /), `P2SH` (m / 49' /), `P2WPKH` (m / 84' /)
@@ -20,6 +22,7 @@ module.exports.getAddressFromXpub = (extPubKey, keyIndex, purpose, accountNumber
   const address = addressFromExtPubKey({
     extPubKey,
     accountNumber: accountNumber || 0,
+    change: change || 0,
     keyIndex,
     purpose: Purpose[purpose],
     network: "mainnet",
@@ -28,12 +31,13 @@ module.exports.getAddressFromXpub = (extPubKey, keyIndex, purpose, accountNumber
   return address
 }
 
-module.exports.getAddressesFromXpub = (extPubKey, addressCount, purpose, keyIndex, accountNumber) => {
+module.exports.getAddressesFromXpub = (extPubKey, addressCount, purpose, keyIndex, accountNumber, change) => {
   
   const address = addressesFromExtPubKey({
-    addressCount,
     extPubKey,
+    addressCount,
     accountNumber: accountNumber || 0,
+    change: change || 0,
     keyIndex,
     purpose: Purpose[purpose],
     network: "mainnet",

ffd35c36ddd920ad10468efa9ffa79dcfda59305
Zane Pocock <zanepocock@gmail.com>
2022-02-05 19:53:55 -0600

    Refactor getTransactionType

--- services/transaction.js
+++ services/transaction.js
@@ -11,6 +11,7 @@ const { checkAndCreateCategory } = require('./category')
 const { checkAndCreateUtxo } = require('./utxo')
 const { getAddressTransactions } = require('./electrum')
 const { formatTransactionsObject } = require('./transactions/formatTransactionsObject')
+const { getTransactionType } = require('./transactions/getTransactionType')
 
 const {Op} = Sequelize
 
@@ -40,41 +41,6 @@ const checkAndCreateAccount = async (name) => {
     accountId = newAccount.dataValues.id
   }
   return accountId
-}
-
-const getTransactionType = async (ledgers) => {
-  const creditsOwned = []
-  const debitsOwned = []
-  for (let i = 0; i < ledgers.length; i += 1) {
-    const account = await db.account.findOne({
-      where: {
-        id: ledgers[i].accountId
-      }
-    })
-    if (!account && ledgers[i].transactiontypeId === 1) {
-      debitsOwned.push(false)
-    }
-    else if (account && ledgers[i].transactiontypeId === 1 && account?.id !== 0) {
-      debitsOwned.push(account?.owned)
-    } else if (!account && ledgers[i].transactiontypeId === 2) {
-      creditsOwned.push(false)
-    } else if (account && ledgers[i].transactiontypeId === 2 && account?.id !== 0) {
-      creditsOwned.push(account?.owned)
-    }
-  }
-  if (!_.includes(creditsOwned, false) && !_.includes(debitsOwned, false)) {
-    return "transfer"
-  }
-  if (_.includes(creditsOwned, true) && !_.includes(debitsOwned, true)) {
-    return "deposit"
-  }
-  if (_.includes(creditsOwned, true) && _.includes(creditsOwned, false) && _.includes(debitsOwned, true) && _.includes(debitsOwned, false)) {
-    return "coinjoin"
-  }
-  if (_.includes(creditsOwned, false) && !_.includes(debitsOwned, false)) {
-    return "withdrawal"
-  }
-  return null
 }
 
 const transactionByUUID = async (id) => {

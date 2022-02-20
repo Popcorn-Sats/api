2a30845283ad674d6e36d81de82df8d24104cbab
Zane Pocock <zanepocock@gmail.com>
2022-02-20 12:04:26 -0600

    Bitcoin/shitcoin "iso" codes

---
+++ utils/isoBitcoin.js
@@ -0,0 +1,11 @@
+const isoBitcoin = [
+  {
+    alphabeticCode: "BTC",
+    minorUnit: 8,
+    entity: "Bitcoin"
+  }
+]
+
+module.exports = {
+  isoBitcoin
+}
---
+++ utils/isoShitcoins.js
@@ -0,0 +1,86 @@
+const isoShitcoins = [
+  {
+    alphabeticCode: "ADA",
+    minorUnit: 6,
+    entity: "Ada"
+  },
+  {
+    alphabeticCode: "BCH",
+    minorUnit: 8,
+    entity: "B Cash"
+  },
+  {
+    alphabeticCode: "BNB",
+    minorUnit: 8,
+    entity: "Binance"
+  },
+  {
+    alphabeticCode: "BSV",
+    minorUnit: 8,
+    entity: "Bitcoin Scammer Vision"
+  },
+  {
+    alphabeticCode: "DASH",
+    minorUnit: 8,
+    entity: "Dash"
+  },
+  {
+    alphabeticCode: "DOGE",
+    minorUnit: 4,
+    entity: "Dogecoin"
+  },
+  {
+    alphabeticCode: "EOS",
+    minorUnit: 4,
+    entity: "EOS"
+  },
+  {
+    alphabeticCode: "ETH",
+    minorUnit: 18,
+    entity: "Etherium"
+  },
+  {
+    alphabeticCode: "LTC",
+    minorUnit: 8,
+    entity: "Litecoin"
+  },
+  {
+    alphabeticCode: "VTC",
+    minorUnit: 8,
+    entity: "Vertcoin"
+  },
+  {
+    alphabeticCode: "XLM",
+    minorUnit: 8,
+    entity: "Stellar Lumen"
+  },
+  {
+    alphabeticCode: "XMR",
+    minorUnit: 12,
+    entity: "Monero"
+  },
+  {
+    alphabeticCode: "XNO",
+    minorUnit: 30,
+    entity: "Nano"
+  },
+  {
+    alphabeticCode: "XRP",
+    minorUnit: 6,
+    entity: "Ripple"
+  },
+  {
+    alphabeticCode: "XTZ",
+    minorUnit: 6,
+    entity: "Tez"
+  },
+  {
+    alphabeticCode: "ZEC",
+    minorUnit: 8,
+    entity: "Zcash"
+  }
+]
+
+module.exports = {
+  isoShitcoins
+}

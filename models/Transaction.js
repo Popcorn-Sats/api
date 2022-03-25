e8d8f2a560a0672cad15cbe8b154118f6de6830f
Zane Pocock <zanepocock@gmail.com>
2022-03-24 22:08:26 -0500

    Refactor Transaction Model

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,92 +1,108 @@
+const Sequelize = require('sequelize')
+const { Model } = require('sequelize')
 const { paginate } = require('../utils/paginate')
+const { getFilter } = require('../utils/getFilter')
 
-module.exports = (sequelize, DataTypes) => {
-  const Transaction = sequelize.define('transaction', {
+module.exports = (sequelize) => {
+
+  class Transaction extends Model {
+      
+      static getAllTransactionsPaginated(page, perPage, sort = 'id', order = 'DESC', filter) {
+        const where = getFilter(filter)
+        return this.findAndCountAll({
+          where,
+          attributes: {
+            include: [
+              [
+                sequelize.literal(`(SELECT height FROM blocks WHERE blocks."id" = transaction."blockId")`), 'blockHeight'
+              ]
+            ]
+          },
+          include: ['category', 'block', 'transactiontype', {model: sequelize.models.transactionledger, include: ['account', 'utxo']}],
+          order: [
+              [[sequelize.literal('"blockHeight"'), 'DESC']],
+              [sort, order],
+          ],
+          distinct: true, // Needed to get correct count
+          ...paginate({ page, perPage })
+        })
+      }
+
+      static findByTransactionId(txid){
+        return this.findOne({ where: { txid } })
+      }
+
+      static getOffsetTransactions(offset, limit){ 
+        return this.findAll({
+          attributes: [
+            'id',
+            [
+              sequelize.literal(`(SELECT height FROM blocks WHERE blocks."id" = transaction."blockId")`), 'blockHeight'
+            ]
+          ],
+          order: [
+            [[sequelize.literal('"blockHeight"'), 'DESC']],
+            ['id', 'DESC'],
+          ],
+          offset,
+          limit,
+        })
+      }
+
+      static getOffsetCategoryTransactions(categoryid, offset, limit){
+        return this.findAll({
+          where: {
+            categoryid
+          },
+          attributes: ['id'],
+          order: [
+            ['id', 'DESC'],
+          ],
+          offset,
+          limit,
+        })
+      }
+  
+      static associate(models) {
+        Transaction.belongsTo(models.category, {
+          foreignKey: 'categoryid',
+          onDelete: 'set null',
+        })
+        // Note: had to change transactions.categoryId column to all lower case due to sequelize.literal bug
+        Transaction.belongsTo(models.transactiontype);
+
+        Transaction.belongsTo(models.block, {
+            foreignKey: 'blockId'
+          });
+
+        Transaction.TransactionLedgers = Transaction.hasMany(models.transactionledger)
+      }
+  }
+
+  Transaction.init({
     // Note: primary key needed in model for Sequelize to merge properly on 'hasMany'
     id: {
-      type: DataTypes.INTEGER,
+      type: Sequelize.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
     txid: {
-      type: DataTypes.STRING,
+      type: Sequelize.STRING,
     },
     network_fee: {
-      type: DataTypes.INTEGER,
+      type: Sequelize.INTEGER,
     },
     size: {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
     },
     description: {
-      type: DataTypes.STRING,
+      type: Sequelize.STRING,
     }
+  }, {
+    sequelize,
+    modelName: 'transaction',
   })
 
-  Transaction.associate = (models) => {
-      Transaction.belongsTo(models.category, {
-          foreignKey: 'categoryid',
-          onDelete: 'set null',
-        });
-      // Note: had to change transactions.categoryId column to all lower case due to sequelize.literal bug
-
-      Transaction.belongsTo(models.transactiontype);
-
-      Transaction.belongsTo(models.block, {
-          foreignKey: 'blockId'
-        });
-
-      Transaction.TransactionLedgers = Transaction.hasMany(models.transactionledger)
-  }
-
-  Transaction.findByTransactionId = (txid) => Transaction.findOne({ where: { txid } })
-
-  Transaction.getAllTransactionsPaginated = (page, perPage, sort = 'id', order = 'DESC', filter) =>
-    Transaction.findAndCountAll({
-      attributes: {
-        include: [
-          [
-            sequelize.literal(`(SELECT height FROM blocks WHERE blocks."id" = transaction."blockId")`), 'blockHeight'
-          ]
-        ]
-      },
-      include: ['category', 'block', 'transactiontype', {model: sequelize.models.transactionledger, include: ['account', 'utxo']}],
-      order: [
-        [[sequelize.literal('"blockHeight"'), 'DESC']],
-        [sort, order],
-      ],
-      distinct: true, // Needed to get correct count
-      ...paginate({ page, perPage })
-    })
-  
-
-  Transaction.getOffsetTransactions = (offset, limit) => 
-    Transaction.findAll({
-      attributes: [
-        'id',
-        [
-          sequelize.literal(`(SELECT height FROM blocks WHERE blocks."id" = transaction."blockId")`), 'blockHeight'
-        ]
-      ],
-      order: [
-        [[sequelize.literal('"blockHeight"'), 'DESC']],
-        ['id', 'DESC'],
-      ],
-      offset,
-      limit,
-    })
-
-  Transaction.getOffsetCategoryTransactions = (categoryid, offset, limit) =>
-    Transaction.findAll({
-      where: {
-        categoryid
-      },
-      attributes: ['id'],
-      order: [
-        ['id', 'DESC'],
-      ],
-      offset,
-      limit,
-    })
-
   return Transaction
+
 }

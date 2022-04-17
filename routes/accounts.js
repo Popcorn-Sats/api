82e5ce452ec96b8ddb6a77d251d0d0bbc72d9c12
Zane Pocock <zanepocock@gmail.com>
2022-04-16 20:53:01 -0500

    verifyToken on accounts routes

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,17 +1,35 @@
 const express = require('express');
 
 const ctrl = require('../controllers/account')
+const { authJwt } = require('../middleware')
 
 const router = express.Router();
 
 router
-  .get('/', ctrl.getAccounts)
-  .put('/:id', ctrl.editAccount)
-  .delete('/:id', ctrl.deleteAccount)
-  .post('/', ctrl.addAccount)
-  .get('/position', ctrl.getPosition)
-  .get('/search', ctrl.searchAccounts)
+  .get(
+    '/',
+    [authJwt.verifyToken],
+    ctrl.getAccounts)
+  .put(
+    '/:id',
+    [authJwt.verifyToken],
+     ctrl.editAccount)
+  .delete('/:id',
+    [authJwt.verifyToken],
+    ctrl.deleteAccount)
+  .post('/',
+    [authJwt.verifyToken],
+    ctrl.addAccount)
+  .get('/position',
+    [authJwt.verifyToken],
+    ctrl.getPosition)
+  .get('/search',
+    [authJwt.verifyToken],
+    ctrl.searchAccounts)
   .get('/scan/:accountId', ctrl.scanOldAccount)
-  .get('/:accountId', ctrl.getSingleAccount)
+  .get(
+    '/:accountId',
+    [authJwt.verifyToken],
+    ctrl.getSingleAccount)
 
 module.exports = router

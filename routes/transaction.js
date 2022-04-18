9b70b3cc3605a087adf726c7ee36ebc3d740bf7a
Zane Pocock <zanepocock@gmail.com>
2022-04-18 16:16:32 -0500

    Verify token on transaction routes

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,17 +1,50 @@
 const express = require('express')
 
+const { authJwt } = require('../middleware')
 const ctrl = require('../controllers/transaction')
 
 const router = express.Router()
 
 router
-  .get('/', ctrl.getTransactions)
-  .get('/search', ctrl.searchTransactions)
-  .get('/account/:accountId', ctrl.getTransactionsForAccount)
-  .get('/category/:categoryId', ctrl.getTransactionsForCategory)
-  .get('/transaction/:transactionId', ctrl.getTransactionByTransactionID)
-  .put('/:id', ctrl.editTransaction)
-  .put('/category', ctrl.editTransactionCategory)
-  .post('/add', ctrl.addTransaction)
+  .get(
+    '/',
+    [authJwt.verifyToken],
+    ctrl.getTransactions
+  )
+  .get(
+    '/search',
+    [authJwt.verifyToken],
+    ctrl.searchTransactions
+  )
+  .get(
+    '/account/:accountId',
+    [authJwt.verifyToken],
+    ctrl.getTransactionsForAccount
+  )
+  .get(
+    '/category/:categoryId',
+    [authJwt.verifyToken],
+    ctrl.getTransactionsForCategory
+  )
+  .get(
+    '/transaction/:transactionId',
+    [authJwt.verifyToken],
+    ctrl.getTransactionByTransactionID
+  )
+  .put(
+    '/:id',
+    [authJwt.verifyToken],
+    ctrl.editTransaction
+  )
+  .put(
+    '/category',
+    [authJwt.verifyToken],
+    ctrl.editTransactionCategory
+  )
+  .post(
+    '/add',
+    [authJwt.verifyToken],
+    ctrl.addTransaction
+  )
 
 module.exports = router;

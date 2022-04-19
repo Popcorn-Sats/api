ec22a0f7f0818160e8346a1db689f46c41a6e0f6
Zane Pocock <zanepocock@gmail.com>
2022-04-19 12:10:40 -0500

    Add verifyToken to Reports route

--- routes/reports.js
+++ routes/reports.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-console */
 const express = require('express')
 
+const { authJwt } = require('../middleware')
 const ctrl = require('../controllers/reports')
 
 const router = express.Router()
 
 router
-  .get('/', ctrl.getReports)
+  .get(
+    '/',
+    [authJwt.verifyToken],
+    ctrl.getReports
+  )
 
 module.exports = router;

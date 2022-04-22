1e2652160b4b8ce294de7c19049aeb209a4e5fa4
Zane Pocock <zanepocock@gmail.com>
2022-04-21 21:22:34 -0500

    DRY ctageory JWT middleware

--- routes/categories.js
+++ routes/categories.js
@@ -7,34 +7,31 @@ const ctrl = require('../controllers/category')
 const router = express.Router()
 
 router
+  .use(
+    authJwt.verifyToken,
+  )
   .get(
     '/',
-    [authJwt.verifyToken],
     ctrl.getCategories
   )
   .put(
     '/:id',
-    [authJwt.verifyToken],
     ctrl.editCategory
   )
   .delete(
     '/:id',
-    [authJwt.verifyToken],
     ctrl.deleteCategory
   )
   .post(
     '/',
-    [authJwt.verifyToken],
     ctrl.addCategory
   )
   .get(
     '/search',
-    [authJwt.verifyToken],
     ctrl.searchCategories
   )
   .get(
     '/:categoryId',
-    [authJwt.verifyToken],
     ctrl.getSingleCategory
   )
 

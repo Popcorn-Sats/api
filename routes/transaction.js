b1a902e5abc454a22525882904f6bd145d1ae026
Zane Pocock <zanepocock@gmail.com>
2022-04-23 12:20:22 -0500

    DRY transactions route middleware

--- routes/transaction.js
+++ routes/transaction.js
@@ -6,44 +6,39 @@ const ctrl = require('../controllers/transaction')
 const router = express.Router()
 
 router
+  .use(
+    authJwt.verifyToken,
+  )
   .get(
     '/',
-    [authJwt.verifyToken],
     ctrl.getTransactions
   )
   .get(
     '/search',
-    [authJwt.verifyToken],
     ctrl.searchTransactions
   )
   .get(
     '/account/:accountId',
-    [authJwt.verifyToken],
     ctrl.getTransactionsForAccount
   )
   .get(
     '/category/:categoryId',
-    [authJwt.verifyToken],
     ctrl.getTransactionsForCategory
   )
   .get(
     '/transaction/:transactionId',
-    [authJwt.verifyToken],
     ctrl.getTransactionByTransactionID
   )
   .put(
     '/:id',
-    [authJwt.verifyToken],
     ctrl.editTransaction
   )
   .put(
     '/category',
-    [authJwt.verifyToken],
     ctrl.editTransactionCategory
   )
   .post(
     '/add',
-    [authJwt.verifyToken],
     ctrl.addTransaction
   )
 

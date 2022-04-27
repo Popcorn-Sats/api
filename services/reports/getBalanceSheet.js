bb265da5720cf66bff119918bbcba009fbd7f6eb
Zane Pocock <zanepocock@gmail.com>
2022-04-26 23:09:37 -0500

    balance sheet decorator

--- services/reports/getBalanceSheet.js
+++ services/reports/getBalanceSheet.js
@@ -6,28 +6,84 @@ const getBalanceSheet = async (userId, year) => {
   const currentYear = new Date().getFullYear()
   const isCurrentYear = year === currentYear
 
-  const accounts = await getAllAccounts() // TODO: filter by userId
+  const organizeAccountsBySubType = (accounts) => {
+    const accountsBySubType = accounts.reduce((acc, account) => {
+      const { reportAccountSubType } = account
+      if (!acc[reportAccountSubType]) {
+        acc[reportAccountSubType] = []
+      }
+      acc[reportAccountSubType].push(account)
+      return acc
+    }, {})
+    return accountsBySubType
+  }
+
+  const accounts = await getAllAccounts() // TODO: filter by userId / entityId
   .catch(err => console.log(err))
   
-  const currentAssets = accounts.filter(account => account.type === reportAccountTypes.CURRENT_ASSET)
+  const currentAssets = accounts.filter(account => account.reportAccountType === reportAccountTypes.CURRENT_ASSET)
+  const currentAssetsBySubType = organizeAccountsBySubType(currentAssets)
+  const currentAssetsBySubTypeSum = Object.keys(currentAssetsBySubType).reduce((acc, curr) => {
+    acc[curr] = currentAssetsBySubType[curr].reduce((acc2, curr2) => acc2 + curr2.balance, 0)
+    return acc
+  }, {})
   const currentAssetsBalance = currentAssets.reduce((acc, curr) => acc + curr.balance, 0)
 
-  const fixedAssets = accounts.filter(account => account.type === 'fixedAsset')
+  const fixedAssets = accounts.filter(account => account.reportAccountType === reportAccountTypes.FIXED_ASSET)
   const fixedAssetsBalance = fixedAssets.reduce((acc, curr) => acc + curr.balance, 0)
 
-  const currentLiabilities = accounts.filter(account => account.type === 'currentLiability')
+  const totalAssets = currentAssetsBalance + fixedAssetsBalance
+
+  const currentLiabilities = accounts.filter(account => account.reportAccountType === reportAccountTypes.CURRENT_LIABILITY)
   const currentLiabilitiesBalance = currentLiabilities.reduce((acc, curr) => acc + curr.balance, 0)
 
-  const longTermLiabilities = accounts.filter(account => account.type === 'longTermLiability')
+  const longTermLiabilities = accounts.filter(account => account.reportAccountType === reportAccountTypes.LONG_TERM_LIABILITY)
   const longTermLiabilitiesBalance = longTermLiabilities.reduce((acc, curr) => acc + curr.balance, 0)
 
-  const equity = accounts.filter(account => account.type === 'equity')
+  const totalLiabilities = currentLiabilitiesBalance + longTermLiabilitiesBalance
+
+  const equity = accounts.filter(account => account.reportAccountType === reportAccountTypes.EQUITY)
   const equityBalance = equity.reduce((acc, curr) => acc + curr.balance, 0)
 
-  const balanceSheet = accounts.map(account => ({
-      name: account.name,
-      balance: account.balance
-    }))
+  const liabilitiesPlusEquity = totalLiabilities + equityBalance
+
+  const check = totalAssets - liabilitiesPlusEquity
+
+  const balanceSheet = {
+    currentAssets: {
+      balance: currentAssetsBalance,
+      accounts: currentAssetsBySubTypeSum
+    },
+    fixedAssets: {
+      balance: fixedAssetsBalance,
+      accounts: fixedAssets
+    },
+    totalAssets: {
+      balance: totalAssets,
+    },
+    currentLiabilities: {
+      balance: currentLiabilitiesBalance,
+      accounts: currentLiabilities
+    },
+    longTermLiabilities: {
+      balance: longTermLiabilitiesBalance,
+      accounts: longTermLiabilities
+    },
+    totalLiabilities: {
+      balance: totalLiabilities,
+    },
+    equity: {
+      balance: equityBalance,
+      accounts: equity
+    },
+    liabilitiesPlusEquity: {
+      balance: liabilitiesPlusEquity,
+    },
+    check: {
+      balance: check,
+    }
+  }
+
   return balanceSheet
 }
 

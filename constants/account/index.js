a5eb0d3a69220a0f746b28565b789a2c68d6c405
Zane Pocock <zanepocock@gmail.com>
2022-04-26 22:18:11 -0500

    Add constants for report account subtypes

--- constants/account/index.js
+++ constants/account/index.js
@@ -28,3 +28,31 @@ module.exports.reportAccountTypes = {
   LONG_TERM_LIABILITY: 'longTermLiability',
   EQUITY: 'equity',
 }
+
+module.exports.reportAccountSubTypes = {
+  // CURRENT_ASSET
+  CASH: 'cash',
+  ACCOUNTS_RECEIVABLE: 'accountsReceivable',
+  INVENTORY: 'inventory',
+  PREPAID_EXPENSES: 'prepaidExpenses',
+  // FIXED_ASSET
+  LAND: 'land',
+  EQUIPMENT: 'equipment',
+  ACCUMULATED_DEPRECIATION: 'accumulatedDepreciation',
+  // CURRENT_LIABILITY
+  ACCOUNTS_PAYABLE: 'accountsPayable',
+  ACCRUED_LIABILITIES: 'accruedLiabilities',
+  DEFFERRED_REVENUES: 'deferredRevenues',
+  // LONG_TERM_LIABILITY
+  NOTES_PAYABLE: 'notesPayable',
+  MORTGAGE: 'mortgage',
+  // EQUITY
+  CAPITAL_STOCK: 'capitalStock',
+  RETURN_ASSET: 'returnAsset',
+  // OTHER
+  OTHER_CURRENT_ASSET: 'otherCurrentAsset',
+  OTHER_FIXED_ASSET: 'otherFixedAsset',
+  OTHER_CURRENT_LIABILITY: 'otherCurrentLiability',
+  OTHER_LONG_TERM_LIABILITY: 'otherLongTermLiability',
+  OTHER_EQUITY: 'otherEquity',
+}

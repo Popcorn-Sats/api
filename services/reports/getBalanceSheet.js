9c3dc9bbd7c0e7bad84dcfc5d4d0ee8f8d3f61e6
Zane Pocock <zanepocock@gmail.com>
2022-04-29 10:51:34 -0500

    Flesh out balance sheet account subtypes

--- services/reports/getBalanceSheet.js
+++ services/reports/getBalanceSheet.js
@@ -30,19 +30,39 @@ const getBalanceSheet = async (userId, year) => {
   const currentAssetsBalance = currentAssets.reduce((acc, curr) => acc + curr.balance, 0)
 
   const fixedAssets = accounts.filter(account => account.reportAccountType === reportAccountTypes.FIXED_ASSET)
+  const fixedAssetsBySubType = organizeAccountsBySubType(fixedAssets)
+  const fixedAssetsBySubTypeSum = Object.keys(fixedAssetsBySubType).reduce((acc, curr) => {
+    acc[curr] = fixedAssetsBySubType[curr].reduce((acc2, curr2) => acc2 + curr2.balance, 0)
+    return acc
+  }, {})
   const fixedAssetsBalance = fixedAssets.reduce((acc, curr) => acc + curr.balance, 0)
 
   const totalAssets = currentAssetsBalance + fixedAssetsBalance
 
   const currentLiabilities = accounts.filter(account => account.reportAccountType === reportAccountTypes.CURRENT_LIABILITY)
+  const currentLiabilitiesBySubType = organizeAccountsBySubType(currentLiabilities)
+  const currentLiabilitiesBySubTypeSum = Object.keys(currentLiabilitiesBySubType).reduce((acc, curr) => {
+    acc[curr] = currentLiabilitiesBySubType[curr].reduce((acc2, curr2) => acc2 + curr2.balance, 0)
+    return acc
+  }, {})
   const currentLiabilitiesBalance = currentLiabilities.reduce((acc, curr) => acc + curr.balance, 0)
 
   const longTermLiabilities = accounts.filter(account => account.reportAccountType === reportAccountTypes.LONG_TERM_LIABILITY)
+  const longTermLiabilitiesBySubType = organizeAccountsBySubType(longTermLiabilities)
+  const longTermLiabilitiesBySubTypeSum = Object.keys(longTermLiabilitiesBySubType).reduce((acc, curr) => {
+    acc[curr] = longTermLiabilitiesBySubType[curr].reduce((acc2, curr2) => acc2 + curr2.balance, 0)
+    return acc
+  }, {})
   const longTermLiabilitiesBalance = longTermLiabilities.reduce((acc, curr) => acc + curr.balance, 0)
 
   const totalLiabilities = currentLiabilitiesBalance + longTermLiabilitiesBalance
 
   const equity = accounts.filter(account => account.reportAccountType === reportAccountTypes.EQUITY)
+  const equityBySubType = organizeAccountsBySubType(equity)
+  const equityBySubTypeSum = Object.keys(equityBySubType).reduce((acc, curr) => {
+    acc[curr] = equityBySubType[curr].reduce((acc2, curr2) => acc2 + curr2.balance, 0)
+    return acc
+  }, {})
   const equityBalance = equity.reduce((acc, curr) => acc + curr.balance, 0)
 
   const liabilitiesPlusEquity = totalLiabilities + equityBalance
@@ -56,25 +76,25 @@ const getBalanceSheet = async (userId, year) => {
     },
     fixedAssets: {
       balance: fixedAssetsBalance,
-      accounts: fixedAssets
+      accounts: fixedAssetsBySubTypeSum
     },
     totalAssets: {
       balance: totalAssets,
     },
     currentLiabilities: {
       balance: currentLiabilitiesBalance,
-      accounts: currentLiabilities
+      accounts: currentLiabilitiesBySubTypeSum
     },
     longTermLiabilities: {
       balance: longTermLiabilitiesBalance,
-      accounts: longTermLiabilities
+      accounts: longTermLiabilitiesBySubTypeSum
     },
     totalLiabilities: {
       balance: totalLiabilities,
     },
     equity: {
       balance: equityBalance,
-      accounts: equity
+      accounts: equityBySubTypeSum
     },
     liabilitiesPlusEquity: {
       balance: liabilitiesPlusEquity,

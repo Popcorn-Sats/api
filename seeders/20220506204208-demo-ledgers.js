12126f73b3212de2b751291b3ffeb2e560951ac7
Zane Pocock <zanepocock@gmail.com>
2022-05-06 15:58:38 -0500

    Create demo transactionledger seed

---
+++ seeders/20220506204208-demo-ledgers.js
@@ -0,0 +1,106 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('transactionledgers', [
+      {
+        amount: 7800,
+        accountId: 5,
+        transactionId: 1,
+        transactiontypeId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 220,
+        accountId: 2,
+        transactionId: 1,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 7580,
+        accountId: 1,
+        transactionId: 1,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 6000,
+        accountId: 1,
+        transactionId: 2,
+        transactiontypeId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 220,
+        accountId: 2,
+        transactionId: 2,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 5780,
+        accountId: 6,
+        transactionId: 2,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 580,
+        accountId: 1,
+        transactionId: 3,
+        transactiontypeId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 220,
+        accountId: 2,
+        transactionId: 3,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 360,
+        accountId: 3,
+        transactionId: 3,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 1000,
+        accountId: 1,
+        transactionId: 4,
+        transactiontypeId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 220,
+        accountId: 2,
+        transactionId: 4,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        amount: 880,
+        accountId: 4,
+        transactionId: 4,
+        transactiontypeId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('transactionledgers', null, {})
+  }
+}

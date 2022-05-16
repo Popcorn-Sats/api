36f51a36b75f7a8b929305202d87cd5fb86c1f8c
Zane Pocock <zanepocock@gmail.com>
2022-05-15 20:16:36 -0500

    Tidy up balance sheet controller

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,4 +1,5 @@
 // const { getAllReportsPaginated } = require('../services/reports')
+const { getBalanceSheet } = require('../services/reports/getBalanceSheet')
 
 const getAllReportsPaginated = async (page, perPage, sort, order, filter) => {
   console.log({page, perPage, sort, order, filter})
@@ -15,11 +16,9 @@ const getReports = async (req, res) => {
 }
 
 const getSingleBalanceSheet = async (req, res) => {
-  const { page, perPage, sort, order, filter } = req.query
-  const parsedFilter = filter ? JSON.parse(filter) : null;
-  const reports = await getAllReportsPaginated(page, perPage, sort, order, parsedFilter)
+  const { userId, year } = req.query
+  const reports = await getBalanceSheet(userId, year)
   .catch(err => res.status(500).send(err))
-  res.header('Content-Range', `bytes : ${(page - 1) * perPage}-${page * perPage - 1}/${reports.count}`)
   return res.json(reports.reports)
 }
 

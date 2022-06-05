b21736fb731be42e612bc28a8fcd5a0d2b7d178e
Zane Pocock <zanepocock@gmail.com>
2022-06-04 20:23:50 -0500

    Move CORS origin to config template

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express')
 
 const app = express()
 const cors = require('cors')
+const config = require('./config/config.json')
 const db = require('./models')
 
 const PORT = process.env.PORT || 2121
 const getRoutes = require('./routes')
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: config.APPLICATION.hostname.admin,
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token', 'Authorization'],
   exposedHeaders: ['Content-Range'],
 }
